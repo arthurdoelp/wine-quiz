b415c063f851cad84dff23dfa90155dc9f4f0700
arthurdoelp <arthurdoelp@gmail.com>
2019-06-30 13:16:11 -0400

    another try

--- server.js
+++ server.js
@@ -37,11 +37,11 @@
 // app.listen(PORT);
 
 const express = require("express");
+const path = require("path");
 
-const mongoose = require("mongoose");
-const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
 const app = express();
-const PORT = process.env.PORT || 3000;
+const apiRoutes = require("./routes/apiRoutes");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -50,13 +50,17 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wine-quiz");
+// Use apiRoutes
+app.use("/api", apiRoutes);
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
+

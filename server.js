c633890064c9ac012361faa06a0c09bf9b3d60e8
arthurdoelp <arthurdoelp@gmail.com>
2019-06-30 12:50:26 -0400

    another try

--- server.js
+++ server.js
@@ -7,18 +7,23 @@ const morgan = require('morgan')
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wine-quiz", { useNewUrlParser: true });
 
-app.use(morgan('dev'));
-    app.use("/public", express.static(path.join(__dirname, 'public')));
-    app.use(
-        bodyParser.urlencoded({
-            extended: false
-        })
-    )
-    app.use(bodyParser.json());
+// Define Middleware Here -------------------------------------
+app.use(morgan('dev'))
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
+//app.use(express.urlencoded({ extended: true }));
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+//app.use(express.json());
+app.use(bodyParser.json());
+
+// Serve Up Static Assets (usually on Heroku) -----------------
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
+}
 
 
 const PORT = process.env.PORT || 3001;

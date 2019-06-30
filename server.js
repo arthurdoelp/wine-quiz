1d2636649c0318ffa90164070b9da52ecbe8a0cc
arthurdoelp <arthurdoelp@gmail.com>
2019-06-30 12:26:12 -0400

    another attempt

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wine-quiz", { useNewUrlParser: true });
 
-if (process.env.NODE_ENV === "production") {
+    app.use("/public", express.static(path.join(__dirname, 'public')));
+    app.use(
+        bodyParser.urlencoded({
+            extended: false
+        })
+    )
+    app.use(bodyParser.json());
   app.use(express.static("client/build"));
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-  });
-}
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);

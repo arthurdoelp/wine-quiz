9136111adac8007380279d34bf2d518d48ee115d
arthurdoelp <arthurdoelp@gmail.com>
2019-06-30 11:28:14 -0400

    updating root / routing

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+app.get('/', (req, res) => {
+    if (process.env.NODE_ENV === "production") {
+        app.use(express.static("client/build"));
+        const path = require('path');
+        app.get('*', (req, res) => {
+          res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        });
+      }
+  });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
@@ -17,6 +19,6 @@ const mongoose = require('mongoose');
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/wine-quiz",
   {
-    //useMongoClient: true
+    useMongoClient: true
   }
 );

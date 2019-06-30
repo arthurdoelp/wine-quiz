81fdca983d40244b5cc33663f91f381bf382f0d4
arthurdoelp <arthurdoelp@gmail.com>
2019-06-30 13:03:53 -0400

    trying reactrouter boilerplate

--- server.js
+++ server.js
@@ -1,37 +1,62 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const bodyParser = require('body-parser');
+// const morgan = require('morgan')
+
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wine-quiz", { useNewUrlParser: true });
+
+// // Define Middleware Here -------------------------------------
+// app.use(morgan('dev'))
+// app.use("/public", express.static(path.join(__dirname, 'public')));
+
+// //app.use(express.urlencoded({ extended: true }));
+// app.use(
+// 	bodyParser.urlencoded({
+// 		extended: false
+// 	})
+// )
+// //app.use(express.json());
+// app.use(bodyParser.json());
+
+// // Serve Up Static Assets (usually on Heroku) -----------------
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+//   });
+// }
+
+
+// // app.get('/', (req, res) => {
+// //     res.send("Hello World!");
+// //   });
+
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT);
+
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
-const path = require('path');
-const bodyParser = require('body-parser');
-const morgan = require('morgan')
+const PORT = process.env.PORT || 3001;
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wine-quiz", { useNewUrlParser: true });
-
-// Define Middleware Here -------------------------------------
-app.use(morgan('dev'))
-app.use("/public", express.static(path.join(__dirname, 'public')));
-
-//app.use(express.urlencoded({ extended: true }));
-app.use(
-	bodyParser.urlencoded({
-		extended: false
-	})
-)
-//app.use(express.json());
-app.use(bodyParser.json());
-
-// Serve Up Static Assets (usually on Heroku) -----------------
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-  });
 }
+// Add routes, both API and view
+app.use(routes);
 
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wine-quiz");
 
-// app.get('/', (req, res) => {
-//     res.send("Hello World!");
-//   });
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT);
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});

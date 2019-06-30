b44b9785b01aac9ea2c2a1e18f610f41b1ba5c92
arthurdoelp <arthurdoelp@gmail.com>
2019-06-30 14:33:11 -0400

    recreated the react app

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          hi
         </p>
         <a
           className="App-link"

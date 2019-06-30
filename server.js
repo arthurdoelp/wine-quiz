4965f2ac65085e3cea9819d757db10c6e140fe36
arthurdoelp <arthurdoelp@gmail.com>
2019-06-30 12:59:01 -0400

    another try

--- server.js
+++ server.js
@@ -23,12 +23,15 @@ app.use(bodyParser.json());
 // Serve Up Static Assets (usually on Heroku) -----------------
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-  });
+// app.get('/', (req, res) => {
+//     res.send("Hello World!");
+//   });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);

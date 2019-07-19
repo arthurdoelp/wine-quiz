2b4f5352cee0de41ed7f46a85ca71b5ea8296f07
arthurdoelp <arthurdoelp@gmail.com>
2019-07-19 11:58:53 -0400

    added units and some images to homepage

---
+++ client/src/components/Units/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+//import UpdateForm from "../components/updateform/index";
+import "./style.css";
+
+function Units(props) {
+    return (
+        <div className="container">
+            <div className="unit-container col-sm-3 col-md-3 col-lg-3">
+                <div className="image-container">
+                    <img className="image" alt={props.title} src={props.image}></img>
+                    <h1 className="number">{props.id}</h1>
+                </div>
+                <h3 className="title">{props.title}</h3>
+            </div>
+
+        </div>
+    );
+}
+
+export default Units;
--- client/src/pages/Lessons/index.js
+++ client/src/pages/Lessons/index.js
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
 //import UpdateForm from "../components/updateform/index";
+import Units from "../../components/Units/index";
+import unitdata from "../../units.json";
 
 class Lessons extends Component {
-  render() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-sm-12 col-md-12 col-lg-12">
-                    <p>hi</p>
+    state = {
+        unitdata
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12 col-md-12 col-lg-12">
+                        <p>hi</p>
+                        {this.state.unitdata.map(unit => (
+                            <Units
+                                id={unit.id}
+                                title={unit.title}
+                                image={unit.image}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default Lessons;

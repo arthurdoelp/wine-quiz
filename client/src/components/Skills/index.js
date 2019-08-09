4569cae6ac04011982820772a03cddc19d994d02
arthurdoelp <arthurdoelp@gmail.com>
2019-08-09 15:22:15 -0400

    Added skills routing

---
+++ client/src/components/Skills/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+//import UpdateForm from "../components/updateform/index";
+import "./style.css";
+
+function Skills(props) {
+    return (
+        <div className="container">
+            <div className="unit-container col-sm-3 col-md-3 col-lg-3">
+                <div className="image-container">
+                    <a href={`/lessons/${props.unit}/${props.title}`}><img className="image" alt={props.title} src={props.image}></img></a>
+                    <h1 className="number">{props.unitId}</h1>
+                </div>
+                <h3 className="title">{props.title}</h3>
+            </div>
+
+        </div>
+    );
+}
+
+export default Skills;
--- client/src/pages/SkillsOverview/index.js
+++ client/src/pages/SkillsOverview/index.js
@@ -1,18 +1,36 @@
 import React, { Component } from "react";
 //import UpdateForm from "../components/updateform/index";
 //import Units from "../../components/Units/index";
-import unitdata from "../../units.json";
+//import unitdata from "../../units.json";
+import skillsdata from "../../skills.json";
+import Skills from "../../components/Skills/index.js";
 
 class SkillsOverview extends Component {
     state = {
-        unitdata
+        skillsdata
     }
+
+    componentDidMount() {
+        const skillsdata = this.state.skillsdata.filter(skill => skill.unit === window.location.pathname.slice(9).replace(/%20/g, " "));
+        this.setState({ skillsdata });
+        console.log(window.location.pathname.slice(9).replace(/%20/g, " "));
+    }
+
     render() {
         return (
                 <div className="container">
                     <div className="row">
                         <div className="col-sm-12 col-md-12 col-lg-12">
                             <p>These are the Skills!</p>
+                            {this.state.skillsdata.map(skill => (
+                                <Skills
+                                    id={skill.id}
+                                    title={skill.title}
+                                    image={skill.image}
+                                    unit={skill.unit}
+                                    unitId={skill.unitId}
+                                />
+                            ))}
                         </div>
                     </div>
                 </div>
